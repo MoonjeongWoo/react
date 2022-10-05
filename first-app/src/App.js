884a133724efb791bdc0e2917ab584b2dc16e369
jeongwoo <mbh219@naver.com>
2022-10-05 17:44:36 +0900

    react 첫 실습

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,24 +1,51 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  // let animal;
+  // let name;
+  // animal = "메롱";
+  // name = "사자";
+  // let a = 5;
+  // let b = 6;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          안녕하세요 만나서 반갑습니다
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div>
+        이것은 div 입니다
+        <h3>이것은 div 안에 있는 h3입니다</h3>
+      </div>
+      <div>
+        <h2>제 반려 동물의 이름은 {name} 입니다</h2>
+        <h2>
+          {name}은 {animal}입니다
+        </h2>
+      </div> */}
+      {/* <div>{3 + 5 == 8 ? "정답입니다" : "오답입니다"}</div> */}
+      {/* <div className="test">Hello world</div>
+      <div className="test02">
+        <div>
+          <label>아이디 :</label>
+          <input></input>
+        </div>
+        <br></br>
+        <label>비밀번호 :</label>
+        <input></input>
+      </div> */}
+      <div className="flex">
+        <div className="red">
+          <div className="eye">
+            <div></div>
+          </div>
+        </div>
+        <div className="orange"></div>
+        <div className="yellow"></div>
+        <div className="green"></div>
+        <div className="blue"></div>
+        <div className="navy"></div>
+        <div className="purple"></div>
+      </div>
+    </>
   );
 }
 
